fdce3782d54074b37470a76d05876f2001badfff
ayxue <ngsbaoyy@hotmail.com>
2021-11-10 10:10:59 +0800

    Remove unused projects

---
+++ src/OpenAPI.Cms.TestTools/Publisher.ExcelAddin/Publisher.ExcelAddin/DB/WebPublisherRepository.cs
@@ -0,0 +1,97 @@
+﻿using Newtonsoft.Json;
+using OpenApi.Cms.TestTools.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenApi.Cms.TestTools.Client.DB
+{
+    public class WebPublisherRepository
+    {
+        private string _connString;
+
+        public WebPublisherRepository(string connString)
+        {
+            _connString = connString;
+        }
+
+        public bool TestDBCOnnection()
+        {
+            using (var conn = new SqlConnection(_connString))
+            {
+                try
+                {
+                    conn.Open();
+                    return true;
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+       
+        public int PublishPositions(GenericCellData cellData)
+        {
+            string spName = "dbo.PublishPositions";
+            using (var conn = new SqlConnection(_connString))
+            {
+                try
+                {                    
+                    conn.Open();
+                    var command = conn.CreateCommand();
+                    command.CommandText = spName;
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandTimeout = 300;
+                    command.Parameters.Add(new SqlParameter("@publishTime", SqlDbType.DateTime2)
+                    {
+                        Value = cellData.Timestamp
+                    });
+
+                    command.Parameters.Add(new SqlParameter("@sheetName", SqlDbType.NVarChar)
+                    {
+                        Value = cellData.SheetName
+                    });
+
+                    command.Parameters.Add(new SqlParameter("@rows", SqlDbType.Int)
+                    {
+                        Value = cellData.Rows
+                    });
+
+                    command.Parameters.Add(new SqlParameter("@columns", SqlDbType.Int)
+                    {
+                        Value = cellData.Columns
+                    });
+
+                    var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(cellData.Data));
+                    command.Parameters.Add(new SqlParameter("@data", SqlDbType.VarBinary)
+                    {
+                        Value = bytes
+                    });
+
+                    return (int)command.ExecuteScalar();
+                }
+                catch
+                {
+                    throw;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ src/OpenAPI.Cms.TestTools/Publisher.ExcelAddin/Publisher.ExcelAddin/UI/PublishTab.cs
@@ -0,0 +1,208 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.Integration.CustomUI;
+using Microsoft.Office.Interop.Excel;
+using Newtonsoft.Json;
+using OpenApi.Cms.TestTools.Client;
+using OpenApi.Cms.TestTools.Client.DB;
+using OpenApi.Cms.TestTools.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Application = Microsoft.Office.Interop.Excel.Application;
+
+namespace Publisher.ExcelAddin
+{
+    [ComVisible(true)]
+    public class PublishTab : ExcelRibbon
+    {
+        private IRibbonUI _ribbon;
+        private Application _app;
+        private List<Worksheet> _fromSheets;
+        private Dictionary<string, string> _sheetsToPublish;
+        private System.Timers.Timer _timer;
+        private bool _isRunning;
+
+        /// <summary>
+        /// Create the Tab
+        /// </summary>
+        /// <param name="RibbonID"></param>
+        /// <returns></returns>
+        public override string GetCustomUI(string RibbonID)
+        {
+            var resourceName = typeof(PublishTab).Namespace + ".UI.PublishTab.xml";
+            using (var stream = typeof(PublishTab).Assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initialize the UI
+        /// </summary>
+        /// <param name="ribbon"></param>
+        public void OnLoad(IRibbonUI ribbon)
+        {
+            // Setup UI
+            _ribbon = ribbon;
+            _ribbon.ActivateTab(ControlID: "tabPublish");
+
+            // Set Application
+            _app = ExcelDnaUtil.Application as Application;
+            if (_app.Workbooks.Count == 0)
+                _app.Workbooks.Add();
+
+            // Set Timer
+            SetTimer();
+        }
+
+        /// <summary>
+        /// Click the Start button
+        /// </summary>
+        /// <param name="control"></param>
+        public void StartPublishPositions(IRibbonControl control)
+        {
+            // Load Sheets
+            if (_fromSheets == null || !_fromSheets.Any())
+            {
+                _fromSheets = new List<Worksheet>();
+                _sheetsToPublish = Config.SheetsToPublish;
+                foreach (Worksheet sheet in _app.Sheets)
+                {
+                    if (_sheetsToPublish.ContainsKey(sheet.Name))
+                        _fromSheets.Add(sheet);
+                }
+            }
+
+            _timer.Enabled = true;
+            _timer.Start();  
+        }
+
+        /// <summary>
+        /// Click the stop button
+        /// </summary>
+        /// <param name="control"></param>
+        public void StopPublishPositions(IRibbonControl control)
+        {
+            _timer.Stop();
+            _timer.Enabled = false;
+            MessageBox.Show($"Publish stopped. ", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        public void TestDbConnection(IRibbonControl control)
+        {
+            try
+            {
+                var dbConn = Config.DbConnectionString;
+                var repo = new WebPublisherRepository(dbConn);
+                repo.TestDBCOnnection();
+                MessageBox.Show($"DB Connected. Connection: {Config.DbConnectionString}", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"DB connection failed. Connection: {Config.DbConnectionString}, Info: {ex.Message}", "Info", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+
+        /// <summary>
+        /// Set the timer
+        /// </summary>
+        private void SetTimer()
+        {
+            var interval = Config.Interval;
+            _timer = new System.Timers.Timer(interval);
+            _timer.Enabled = false;
+            _timer.Elapsed += (s, e) => {
+                PublishSheets();
+            };
+        }
+
+        /// <summary>
+        /// Log
+        /// </summary>
+        /// <param name="message"></param>
+        private void ShowMessage(string message)
+        {
+            //_fromSheet..Cells[0, 0].Value2 = message;
+        }
+
+
+        private void PublishSheets()
+        {
+            try
+            {
+                if (_fromSheets == null || !_fromSheets.Any() || _isRunning)
+                    return;
+
+                _isRunning = true;
+                foreach (var sheet in _fromSheets)
+                {
+                    string rangeName;
+                    if (!_sheetsToPublish.TryGetValue(sheet.Name, out rangeName))
+                        rangeName = string.Empty;
+
+                    var data = GetCellData(sheet, rangeName);
+                    PublishToDB(data);
+                }
+                _isRunning = false;
+            }
+            catch (Exception ex)
+            {
+                _isRunning = false;
+            }
+        }
+
+        /// <summary>
+        /// Get data ona sheet
+        /// </summary>
+        /// <param name="fromSheet"></param>
+        /// <returns></returns>
+        private GenericCellData GetCellData(Worksheet fromSheet, string rangeName)
+        {
+            var sheetName = fromSheet.Name;
+            var usedRange = string.IsNullOrEmpty(rangeName)?fromSheet.UsedRange : fromSheet.Range[rangeName];
+            var val = usedRange.Value2 as object[,];
+
+            var cols = val.GetLength(1);
+            var rows = val.Length / cols;
+
+            return new GenericCellData
+            {
+                Timestamp = DateTime.Now,
+                Rows = rows,
+                Columns = cols,
+                Data = val,
+                SheetName = sheetName 
+            };
+
+            //var form = new FormCopyData();
+            //form.Data = JsonConvert.SerializeObject(data, Formatting.Indented);
+            //form.CellData = data;
+            //form.ShowDialog();
+        }
+
+        /// <summary>
+        /// Publish to DB
+        /// </summary>
+        /// <param name="cellData"></param>
+        /// <returns></returns>
+        /// 
+        private int PublishToDB(GenericCellData cellData)
+        {
+            var dbConn = Config.DbConnectionString;
+            var repo = new WebPublisherRepository(dbConn);
+            return repo.PublishPositions(cellData);
+        }
+    }
+}
