51061b11adc776eef83e22de0ff5c9ac422f4697
ayxue <ngsbaoyy@hotmail.com>
2022-08-07 11:16:11 +0800

    Solve the unstable publish error. Push a message to Excel to callback the publish function when COM is not ready

--- src/OpenAPI.Cms.TestTools/Publisher.ExcelAddin/Publisher.ExcelAddin/UI/PublishRunner.cs
+++ src/OpenAPI.Cms.TestTools/Publisher.ExcelAddin/Publisher.ExcelAddin/UI/PublishRunner.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Publisher.ExcelAddin.UI
 {
@@ -58,36 +59,57 @@ namespace Publisher.ExcelAddin.UI
 
         private void PublishSheets()
         {
-            if (_isRunning)
-                return;
-
-            // 0x8001010A errors were happened when accessing COM at the time Excel is not "Ready". And the only suggestion by COM is "RETRY LATER"...
-            // To avoid such conflicts, COM actions are synchronized by posting a WM_SYNCMACRO windows message. Excel will then receive the message from Windows in its event loop when it is "Ready" and perform the action. 
-            // This feature is avaible in ExcelDNA - ExcelAsyncUtil.QueueAsMacro. Details: https://docs.excel-dna.net/performing-asynchronous-work/
-            // https://stackoverflow.com/questions/25434845/disposing-of-exceldnautil-application-from-new-thread
-            ExcelAsyncUtil.QueueAsMacro(() =>
+            try
             {
-                try
+                // publishing data in this thread directly.
+                // COM will reject access and throw an Exception when COM is no Ready when cursor is outside Excel ("0x8001010A Apllication is busy. " errors and the only suggestion from COM is Retry Later)
+                PublishData();
+            }
+            catch (Exception ex)
+            {
+                // In case of COM rejects access, push a WM_SYNCMACRO windows event to Excel and Excel will call back the publish function when COM is ready
+                // "0x8001010A Apllication is busy" errors were happened when accessing COM at the time Excel is not "Ready". And the only suggestion by COM is "RETRY LATER"...
+                // To avoid such conflicts, COM actions are synchronized by posting a WM_SYNCMACRO windows message. Excel will then receive the message from Windows in its event loop when it is "Ready" and perform the action. 
+                // This feature is avaible in ExcelDNA - ExcelAsyncUtil.QueueAsMacro. Details: https://docs.excel-dna.net/performing-asynchronous-work/
+                // https://stackoverflow.com/questions/25434845/disposing-of-exceldnautil-application-from-new-thread
+                ExcelAsyncUtil.QueueAsMacro(() =>
                 {
-                    // Check Can Publish
-                    if (!_reportConfig.CanPublish())
-                        return;
-
-                    // Collect data and publish
-                    _isRunning = true;
-                    var data = GetCellData(_sheet, _reportConfig.Range);
-
-                    // Publish to DB in an async way
-                    PublishToDBAsync(data);
-                    _isRunning = false;
-                }
-                catch (Exception ex)
-                {
-                    _isRunning = false;
-                }
-            });
-
+                    try
+                    {
+                        PublishData();
+                    }
+                    catch (Exception e)
+                    {
+                        _isRunning = false;
+                    }
+                });
+            }
         }
+
+        private void PublishData()
+        {
+            try
+            {
+                // Check Can Publish
+                if (!_reportConfig.CanPublish())
+                    return;
+
+                // Collect data and publish
+                _isRunning = true;
+                var data = GetCellData(_sheet, _reportConfig.Range);
+
+                // Publish to DB in an async way
+                PublishToDBAsync(data);
+                _isRunning = false;
+            }
+            catch (Exception ex)
+            {
+                _isRunning = false;
+                throw ex;
+            }
+        }
+
+
 
         /// <summary>
         /// Get data ona sheet
@@ -132,7 +154,18 @@ namespace Publisher.ExcelAddin.UI
             {
                 var dbConn = _reportConfig.DbConnectionString;
                 var repo = new WebPublisherRepository(dbConn);
-                repo.PublishPositions(cellData);
+                return repo.PublishPositions(cellData);
+            }).ContinueWith( updateTask =>
+            {
+                try 
+                {
+                    if (updateTask.Exception != null)
+                        throw updateTask.Exception;
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message + ex.StackTrace, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
             });
         }
 
